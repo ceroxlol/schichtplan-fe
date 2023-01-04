9a37845a4584dde8f987b056d5fc6e4022c9fa85
Christopher Werner <christopher.werner@netlight.com>
2023-01-04 22:16:57 +0100

    Add registerUser functionality.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,27 +2,54 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 async function loginUser(credentials) {
-  return fetch('http://localhost:8080/users/login', {
+  return fetch('http://0.0.0.0:8080/users/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(credentials)
   })
-    .then(data => data.json())
+    .then(res =>
+      res.json())
+}
+
+async function registerUser(credentials) {
+  return fetch('http://0.0.0.0:8080/users/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(res =>
+      res.json())
 }
 
 export default function Login({ setToken }) {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [username, setUsername] = useState();
 
-  const handleSubmit = async e => {
+  const handleSubmitLogin = async e => {
     e.preventDefault();
     const token = await loginUser({
-      email: email,
-      password
+      "user": {
+        "email": email,
+        "password": password
+      }
     });
     setToken(token);
+  }
+
+  const handleSubmitRegister = async e => {
+    e.preventDefault();
+    const token = await registerUser({
+      "user": {
+        "email": email,
+        "username": username,
+        "password": password
+      }
+    })
   }
 
   let [authMode, setAuthMode] = useState("signin")
@@ -34,7 +61,7 @@ export default function Login({ setToken }) {
   if (authMode === "signin") {
     return (
       <div className="Auth-form-container">
-        <form className="Auth-form">
+        <form className="Auth-form" onSubmit={handleSubmitLogin}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="text-center">
@@ -49,6 +76,7 @@ export default function Login({ setToken }) {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group mt-3">
@@ -57,6 +85,7 @@ export default function Login({ setToken }) {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
@@ -75,7 +104,7 @@ export default function Login({ setToken }) {
 
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={handleSubmitRegister}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="text-center">
@@ -87,9 +116,10 @@ export default function Login({ setToken }) {
           <div className="form-group mt-3">
             <label>Full Name</label>
             <input
-              type="email"
+              type="text"
               className="form-control mt-1"
               placeholder="e.g Jane Doe"
+              onChange={e => setUsername(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -98,6 +128,7 @@ export default function Login({ setToken }) {
               type="email"
               className="form-control mt-1"
               placeholder="Email Address"
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -106,6 +137,7 @@ export default function Login({ setToken }) {
               type="password"
               className="form-control mt-1"
               placeholder="Password"
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
