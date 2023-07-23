b478dd27bc1afe391acf423c1b3e665978e081ef
Christopher Werner <christopher.werner1@gmx.net>
2023-07-23 14:08:17 +0200

    Cleanup unused code.

--- src/config.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-
-export const defaultTimeStart = moment().startOf('day');
-export const defaultTimeEnd = moment()
-  .add(5, 'year');
-export const interval = 12 * 60 * 60 * 1000;
-export const sidebarWidth = 250;
-export const lineHeight = 50;
